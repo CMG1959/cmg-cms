6756c0a50be4590a4224c0c4b40fd9037abe06c9
mikecaro <mc947@drexel.edu>
2015-06-18 10:10:23 -0400

    comment urls for inspection report

--- inspection/urls.py
+++ inspection/urls.py
@@ -3,16 +3,18 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # ex: /startupshot/
+    ### link to display active jobs
     url(r'^$', views.view_index, name='index'),
+    ### link to job report
     url(r'^jobReport/$', views.view_jobReportSearch, name='view_jobReportSearch'),
+    ### link to item report
     url(r'^itemReport/$', views.view_itemReportSearch, name='view_itemReportSearch'),
-    # ex: /startupshot/123-456789/
+    ### link to view for job report
     url(r'^jobReport/(?P<jobNumber>[0-9]+(.[0-9]+)+)/$', views.view_jobReport, name='view_jobReport'),
+    ### link to show which inspections can be performed on a given job
     url(r'^(?P<jobNumber>[0-9]+(.[0-9]+)+)/$', views.view_detailJob, name='view_detailJob'),
-    # # ex: /startupshot/123-456789/results/
+    ### link to visual inspection form
     url(r'^(?P<jobNumber>[0-9]+(.[0-9]+)+)/visual/$', views.view_visualInspection, name='view_visualInspection'),
-    # # ex: /startupshot/123-456789/vote/
+    ### link to weight inspection form
     url(r'^(?P<jobNumber>[0-9]+(.[0-9]+)+)/weight/$', views.view_weightInspection, name='view_weightInspection'),
-
 ]
