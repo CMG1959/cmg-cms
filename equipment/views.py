f19b1f23b01ed26895e476510137520492f0323f
mikecaro <mc947@drexel.edu>
2015-06-25 12:01:33 -0400

    Initialize equipment information page

--- equipment/views.py
+++ equipment/views.py
@@ -2,8 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.shortcuts import render
 
-
-from models import EquipmentType, EquipmentInfo
+from models import EquipmentType, EquipmentInfo, PM
 
 # Create your views here.
 
@@ -22,6 +21,7 @@ def view_index(request):
 def view_equipment(request,equip_type):
     equipmentTypes = EquipmentInfo.objects.filter(equipment_type__equipment_type=equip_type)
 
+
     # active_parts = Production.objects.filter(inProduction=True).select_related('item')
 
     template = loader.get_template('equipment/equipment_index.html')
@@ -32,9 +32,12 @@ def view_equipment(request,equip_type):
 
 def view_equipment_info(request,equip_type,equip_name):
     equip_info = EquipmentInfo.objects.get(equipment_type__equipment_type=equip_type,part_identifier=equip_name)
+    PMinfo = PM.objects.filter(equipment_type__equipment_type=equip_type).select_related()
+    print PMinfo
 
     template = loader.get_template('equipment/equipment_info.html')
     context = RequestContext(request, {
         'equip_info': equip_info,
+        'PM_info': PMinfo,
         })
     return HttpResponse(template.render(context))
