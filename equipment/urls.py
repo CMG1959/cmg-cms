4e76abba3db5fff7744f0457a63832b49e97a225
Michael Caro <mcaro1987@gmail.com>
2015-06-26 13:29:05 -0400

    Fix regexp for string with spaces

--- equipment/urls.py
+++ equipment/urls.py
@@ -6,15 +6,15 @@ urlpatterns = [
     ### link to view equipment types
     url(r'^$', views.view_index, name='view_equipmentIndex'),
     ### link to view specific equipment of certain type
-    url(r'^(?P<equip_type>\w+)$', views.view_equipment, name='view_equipment'),
+    url(r'^(?P<equip_type>[\w|\W]+)$', views.view_equipment, name='view_equipment'),
     ### Display information related to specific piece of equipment
-    url(r'^(?P<equip_type>\w+)/(?P<equip_name>\w+)$', views.view_equipment_info, name='view_equipment_info'),
+    url(r'^(?P<equip_type>[\w|\W]+)/(?P<equip_name>[\w|\W]+)$', views.view_equipment_info, name='view_equipment_info'),
     ### Go to PM page
-    url(r'^(?P<equip_type>\w+)/(?P<equip_name>\w+)/PM/(?P<pm_type>\w+)$', views.view_pm_form, name='view_pm_form'),
+    url(r'^(?P<equip_type>[\w|\W]+)/(?P<equip_name>[\w|\W]+)/PM/(?P<pm_type>[\w|\W]+)$', views.view_pm_form, name='view_pm_form'),
     ### Go to PM Report
-    url(r'^(?P<equip_type>\w+)/(?P<equip_name>\w+)/ViewPM$', views.view_pm_report, name='view_pm_report'),
+    url(r'^(?P<equip_type>[\w|\W]+)/(?P<equip_name>[\w|\W]+)/ViewPM$', views.view_pm_report, name='view_pm_report'),
     ### Go to Repair form
-    url(r'^(?P<equip_type>\w+)/(?P<equip_name>\w+)/Repair$', views.view_repair_form, name='view_repair_form'),
+    url(r'^(?P<equip_type>[\w|\W]+)/(?P<equip_name>[\w|\W]+)/Repair$', views.view_repair_form, name='view_repair_form'),
     ### link to item report
     # url(r'^itemReport/$', views.view_itemReportSearch, name='view_itemReportSearch'),
     # ### link to view for job report
