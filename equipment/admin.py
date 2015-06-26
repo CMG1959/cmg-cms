4fdb81da63c879e136c1a267801b81f065f492fe
Michael Caro <mcaro1987@gmail.com>
2015-06-26 10:40:23 -0400

    Add equipment repair admin and fix repair model

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import EquipmentType, EquipmentManufacturer, EquipmentInfo, PM, PMFreq, EquipmentPM
+from .models import EquipmentType, EquipmentManufacturer, EquipmentInfo, PM, PMFreq, EquipmentPM, EquipmentRepair
 
 
 
@@ -14,3 +14,4 @@ admin.site.register(EquipmentInfo)
 admin.site.register(PM)
 admin.site.register(PMFreq)
 admin.site.register(EquipmentPM)
+admin.site.register(EquipmentRepair)
