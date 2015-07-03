8daa50943856861c1cd97858d4f78ead2899d6e6
mikecaro <mc947@drexel.edu>
2015-07-03 08:11:54 -0400

    Fix URLs for equipment and continue bootcard merge

--- equipment/urls.py
+++ equipment/urls.py
@@ -4,16 +4,20 @@ from . import views
 
 urlpatterns = [
     ### link to view equipment types
-    url(r'^$', views.view_index, name='view_equipmentIndex'),
+    url(r'^/?$', views.view_index, name='view_equipmentIndex'),
     ### link to view specific equipment of certain type
-    url(r'^(?P<equip_type>[\w|\W]+)$', views.view_equipment, name='view_equipment'),
+    url(r'^(?P<equip_type>[^/]+)/?$', views.view_equipment, name='view_equipment'),
     ### Display information related to specific piece of equipment
-    url(r'^(?P<equip_type>[\w|\W]+)/(?P<equip_name>[\w|\W]+)$', views.view_equipment_info, name='view_equipment_info'),
+    # url(r'^(?P<equip_type>[\w|\W]+)/(?P<equip_name>[\w|\W]+)$', views.view_equipment_info, name='view_equipment_info'),
+    url(r'^(?P<equip_type>[^/]+)/(?P<equip_name>[^/]+)/?$', views.view_equipment_info, name='view_equipment_info'),
     ### Go to PM page
-    url(r'^(?P<equip_type>[\w|\W]+)/(?P<equip_name>[\w|\W]+)/PM/(?P<pm_type>[\w|\W]+)$', views.view_pm_form,
+    url(r'^(?P<equip_type>[^/]+)/(?P<equip_name>[^/]+)/PM/(?P<pm_type>[^/]+)/?$', views.view_pm_form,
         name='view_pm_form'),
     ### Go to PM Report
-    url(r'^(?P<equip_type>[\w|\W]+)/(?P<equip_name>[\w|\W]+)/ViewPM$', views.view_pm_report, name='view_pm_report'),
+    url(r'^(?P<equip_type>[^/]+)/(?P<equip_name>[^/]+)/ViewPM$', views.view_pm_report, name='view_pm_report'),
+    ### Go to Repair report
+    url(r'^(?P<equip_type>[^/]+)/(?P<equip_name>[^/]+)/ViewRepair$', views.view_repair_report,
+        name='view_repair_report'),
     ### Go to Repair form
-    url(r'^(?P<equip_type>[\w|\W]+)/(?P<equip_name>[\w|\W]+)/Repair$', views.view_repair_form, name='view_repair_form'),
+    url(r'^(?P<equip_type>[^/]+)/(?P<equip_name>[^/]+)/Repair$', views.view_repair_form, name='view_repair_form'),
 ]
