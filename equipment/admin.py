7ad0b5440ba969d6fde16d484589ac1ed718e855
Michael Caro <mcaro1987@gmail.com>
2015-07-21 15:09:25 -0400

    Add admin fields for equipment

--- equipment/admin.py
+++ equipment/admin.py
@@ -3,10 +3,26 @@ from django.contrib import admin
 # Register your models here.
 from .models import EquipmentType, EquipmentManufacturer, EquipmentInfo, PM, PMFreq, EquipmentPM, EquipmentRepair
 
+class EquipInfoAdmin(admin.ModelAdmin):
+    search_fields = ['part_identifier']
+    list_display = ('equipment_type','part_identifier','manufacturer_name')
+
+class PMAdmin(admin.ModelAdmin):
+    list_display = ('equipment_type','pm_frequency','pm_item')
+
+
+class EquipmentPMAdmin(admin.ModelAdmin):
+    search_fields = ['equipment_ID__part_identifier']
+    list_display = ('employee','dateCreated','equipment_ID','pm_frequency')
+
+class EquipmentRepairAdmin(admin.ModelAdmin):
+    search_fields = ['equipment_ID__part_identifier','po_num']
+    list_display = ('employee','dateCreated','equipment_ID','po_num')
+
 admin.site.register(EquipmentType)
 admin.site.register(EquipmentManufacturer)
-admin.site.register(EquipmentInfo)
-admin.site.register(PM)
+admin.site.register(EquipmentInfo,EquipInfoAdmin)
+admin.site.register(PM,PMAdmin)
 admin.site.register(PMFreq)
-admin.site.register(EquipmentPM)
-admin.site.register(EquipmentRepair)
+admin.site.register(EquipmentPM,EquipmentPMAdmin)
+admin.site.register(EquipmentRepair,EquipmentRepairAdmin)
