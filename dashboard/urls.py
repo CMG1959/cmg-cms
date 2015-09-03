f762033acbb4aa5b4128b7ee2249668fd24b8cb4
Michael Caro <mcaro1987@gmail.com>
2015-09-03 09:54:08 -0400

    Start a part inspection report for wiz

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     # View error log
     url(r'^errorLog/$', views.view_errorLog, name='view_errorLog'),
+    # View error log
+    url(r'^inspectionLog/$', views.view_Inspections, name='view_inspectionLog'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,9 @@ from models import errorLog
 import datetime
 from django.http import HttpResponse, HttpResponseRedirect,Http404
 from django.template import RequestContext, loader
+
+from startupshot.models import MattecProd,startUpShot
+from inspection.models import passFailByPart, rangeTestByPart, textRecordByPart
 from django.shortcuts import render
 # Create your views here.
 
@@ -13,4 +16,32 @@ def view_errorLog(request):
     context = RequestContext(request, {
         'prodErrors':production_errors
     })
+    return HttpResponse(template.render(context))
+
+
+
+def view_Inspections(request):
+
+    jobList = MattecProd.objects.all().values_list('jobNumber', flat=True)
+    n=0
+    resultDict = {}
+    for eachJob in jobList:
+        if startUpShot.objects.filter(jobNumber=eachJob).exists():
+            thisSUS = startUpShot.objects.get(jobNumber=eachJob)
+            pfTests = passFailByPart.objects.filter(item_Number = thisSUS.item)
+            rangeTests = rangeTestByPart.objects.filter(item_Number = thisSUS.item)
+            textTests = textRecordByPart.objects.filter(item_Number = thisSUS.item)
+
+            resultDict[str(n)] = {
+                'susInfo':thisSUS,
+                'pfTests':pfTests,
+                'rangeTests':rangeTests,
+                'textTests':textTests
+            }
+
+            n+=1
+
+
+    template = loader.get_template('dashboard/completedInspections.html')
+    context = RequestContext(request, {'resultDict':resultDict})
     return HttpResponse(template.render(context))
