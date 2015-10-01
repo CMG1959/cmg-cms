a19b128a4affe3b76962b52f66c9b5d0dc31af07
Michael Caro <mcaro1987@gmail.com>
2015-10-01 00:32:08 -0400

    Change model field from 50 characters to 75

--- dashboard/models.py
+++ dashboard/models.py
@@ -8,5 +8,5 @@ class errorLog(models.Model):
     machNo = models.CharField(verbose_name="Machine Number", max_length=10)
     partDesc = models.CharField(verbose_name="Part Description", max_length=50)
     jobID = models.CharField(verbose_name="Job ID", max_length=10)
-    inspectionName = models.CharField(verbose_name = 'Inspection Name', max_length=50)
+    inspectionName = models.CharField(verbose_name = 'Inspection Name', max_length=75)
     errorDescription = models.CharField(verbose_name="Error Description", max_length=250)
