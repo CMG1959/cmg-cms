4d8c51034d710c16a34c221be0c9af82615a619c
mikecaro <mc947@drexel.edu>
2015-10-14 20:56:23 -0700

    New fields for employee table and removed job table

--- employee/models.py
+++ employee/models.py
@@ -2,28 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-class JobTitles(models.Model):
-    class Meta:
-        verbose_name = 'Job Titles'
-        verbose_name_plural = 'Organizations'
-
-    JobNum = models.IntegerField(verbose_name='Job Number')
-    JobTitle = models.CharField(verbose_name='Job Title', max_length=25)
-
-    def __unicode__(self):
-        return '%s' % (self.JobTitle)
-
 class Employees(models.Model):
     class Meta:
         verbose_name = 'Employee Information'
         verbose_name_plural = 'Employee Information'
 
-    EmpNum = models.CharField(verbose_name='Employee Number', max_length=25)
+    EmpNum = models.IntegerField(verbose_name='Employee Number', null=True)
     EmpLName = models.CharField(verbose_name='Last Name', max_length=25, null=True)
     EmpFName = models.CharField(verbose_name='First Name', max_length=25, null=True)
-    EmpManNum = models.CharField(verbose_name='Employee Man Number', max_length=25, null=True)
-    EmpJob = models.ForeignKey('JobTitles',verbose_name='Employee Job Title')
-    EmpShift = models.IntegerField(verbose_name='Shift', null=True)
+    EmpManNum = models.IntegerField(verbose_name='Employee Man Number', null=True)
+    EmpShift = models.CharField(verbose_name='Shift', null=True)
+    StatusActive =  models.BooleanField(verbose_name="Status Active",default=True)
+    IsAdmin = models.BooleanField(verbose_name="Is Admin",default=False)
+    IsQCStaff = models.BooleanField(verbose_name="Is QC Staff",default=False)
+    IsMgmtStaff = models.BooleanField(verbose_name="Is Mgmt Staff",default=False)
+    IsSupervStaff = models.BooleanField(verbose_name="Is Supervisor Staff",default=False)
+    IsOpStaff = models.BooleanField(verbose_name="Is Ops Staff",default=False)
+    IsToolStaff = models.BooleanField(verbose_name="Is Tool Staff",default=False)
+    IsMaintStaff = models.BooleanField(verbose_name="Is Maint Staff",default=False)
+    IsPTLStaff = models.BooleanField(verbose_name="Is PTL Staff",default=False)
 
     def __unicode__(self):
         return '%s - %s' % (self.EmpLName,self.EmpFName)
