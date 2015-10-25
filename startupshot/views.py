e8790e3dd8bd9abc20039735d1d2aa5de7aa40cd
mikecaro <mc947@drexel.edu>
2015-10-25 16:09:04 -0700

    Add FAS01 to startup shot

--- startupshot/views.py
+++ startupshot/views.py
@@ -106,6 +106,11 @@ def createNewStartUpShot(request, jobNo):
             redirect_url = '/startupshot/%s/viewCreated' % (MattecInfo.itemNo.strip())
             return HttpResponseRedirect(redirect_url)
 
+        elif MattecInfo.machNo == 'FAS01':
+            template = loader.get_template('startupshot/viewFAS.html')
+            context = RequestContext(request)
+            return HttpResponse(template.render(context))
+
         else:
             form = startupShotForm()
             form.fields["machineOperator"].queryset = Employees.objects.filter(IsOpStaff=True).order_by('EmpLName')
