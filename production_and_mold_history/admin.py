aaeee6ff1a6bb35e798ee9cd80aac6814e1d3760
mikecaro <mc947@drexel.edu>
2015-12-11 07:10:43 -0800

    Change phl admin

--- production_and_mold_history/admin.py
+++ production_and_mold_history/admin.py
@@ -3,5 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import ProductionHistory, MoldHistory
 
-admin.site.register(ProductionHistory)
-admin.site.register(MoldHistory)
+class ProductionHistoryAdmin(admin.ModelAdmin):
+    search_fields = ['dateCreated', 'jobNumber' 'inspectorName', 'descEvent']
+    list_display = ('dateCreated', 'jobNumber' 'inspectorName', 'descEvent')
+
+class MoldHistoryAdmin(admin.ModelAdmin):
+    search_fields = ['dateCreated', 'moldNumber' 'inspectorName','pm', 'descEvent']
+    list_display = ('dateCreated', 'moldNumber' 'inspectorName','pm', 'descEvent')
+
+admin.site.register(ProductionHistory, ProductionHistoryAdmin)
+admin.site.register(MoldHistoryAdmin, MoldHistory)
