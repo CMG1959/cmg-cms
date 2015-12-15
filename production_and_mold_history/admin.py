73a25bcb14f3de27db9849b036dd76ee3d48f87f
mikecaro <mc947@drexel.edu>
2015-12-15 08:09:17 -0800

    Remove job info from mold report

--- production_and_mold_history/admin.py
+++ production_and_mold_history/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ProductionHistory, MoldHistory
+from .models import ProductionHistory, MoldHistory, MaintanenceRequests
 
 class ProductionHistoryAdmin(admin.ModelAdmin):
+    search_fields = ['dateCreated', 'jobNumber','inspectorName', 'descEvent','notifyToolroom']
+    list_display = ('dateCreated', 'jobNumber', 'inspectorName', 'descEvent','notifyToolroom')
+
+
+class MaintanenceRequestAdmin(admin.ModelAdmin):
     search_fields = ['dateCreated', 'jobNumber','inspectorName', 'descEvent']
     list_display = ('dateCreated', 'jobNumber', 'inspectorName', 'descEvent')
+
 
 class MoldHistoryAdmin(admin.ModelAdmin):
     search_fields = ['dateCreated', 'moldNumber', 'inspectorName','pm', 'descEvent']
@@ -13,3 +19,4 @@ class MoldHistoryAdmin(admin.ModelAdmin):
 
 admin.site.register(ProductionHistory, ProductionHistoryAdmin)
 admin.site.register(MoldHistory, MoldHistoryAdmin)
+admin.site.register(MaintanenceRequests, MaintanenceRequestAdmin)
