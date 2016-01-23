1103df10a9d4fbddaf2e513eca8a85ef381a53e9
mikecaro <mc947@drexel.edu>
2016-01-23 14:09:40 -0800

    forgot key

--- updater.py
+++ updater.py
@@ -1,5 +1,15 @@
-from inspection.models import rangeInspection
+from inspection.models import rangeInspection, passFailInspection, textInspection
 
 for my_inspection in rangeInspection.objects.all():
+    if rangeInspection.headCavID:
+        my_inspection.headCav = rangeInspection.headCavID
+
+
+for my_inspection in passFailInspection.objects.all():
+    if rangeInspection.headCavID:
+        my_inspection.headCav = rangeInspection.headCavID
+
+
+for my_inspection in textInspection.objects.all():
     if rangeInspection.headCavID:
         my_inspection.headCav = rangeInspection.headCavID
