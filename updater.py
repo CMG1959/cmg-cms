c6aab367af0640c200f11fef4319c9dff729b970
Michael Caro <mcaro1987@gmail.com>
2016-01-30 13:00:39 -0500

    Fix mold look up

--- updater.py
+++ updater.py
@@ -1,22 +1,38 @@
-from inspection.models import rangeInspection, passFailInspection, textInspection
+# from inspection.models import rangeInspection, passFailInspection, textInspection
+#
+# for my_inspection in rangeInspection.objects.filter(headCavID__isnull=False):
+#     if my_inspection.headCavID:
+#         my_inspection.headCav = '%s - %s'% (my_inspection.headCavID.head_code.upper()
+#                                             ,my_inspection.headCavID.cavity_id.capitalize())
+#         my_inspection.save()
+#
+#
+# for my_inspection in passFailInspection.objects.filter(headCavID__isnull=False):
+#     if my_inspection.headCavID:
+#         my_inspection.headCav = '%s - %s'% (my_inspection.headCavID.head_code.upper()
+#                                             ,my_inspection.headCavID.cavity_id.capitalize())
+#         my_inspection.save()
+#
+#
+# for my_inspection in textInspection.objects.filter(headCavID__isnull=False):
+#
+#     if my_inspection.headCavID:
+#         my_inspection.headCav = '%s - %s'% (my_inspection.headCavID.head_code.upper()
+#                                             ,my_inspection.headCavID.cavity_id.capitalize())
+#         my_inspection.save()
 
-for my_inspection in rangeInspection.objects.filter(headCavID__isnull=False):
-    if my_inspection.headCavID:
-        my_inspection.headCav = '%s - %s'% (my_inspection.headCavID.head_code.upper()
-                                            ,my_inspection.headCavID.cavity_id.capitalize())
-        my_inspection.save()
+from employee.models import WebAppEmployee, Employees
+from django.contrib.auth.models import User
 
+for employee in Employees.objects.all():
+    try:
+        fname = employee.EmpFName
+        lname = employee.EmpLName
+        man_num = employee.EmpManNum
 
-for my_inspection in passFailInspection.objects.filter(headCavID__isnull=False):
-    if my_inspection.headCavID:
-        my_inspection.headCav = '%s - %s'% (my_inspection.headCavID.head_code.upper()
-                                            ,my_inspection.headCavID.cavity_id.capitalize())
-        my_inspection.save()
+        user_obj = User.objects.get(first_name=fname,last_name=lname)
 
-
-for my_inspection in textInspection.objects.filter(headCavID__isnull=False):
-
-    if my_inspection.headCavID:
-        my_inspection.headCav = '%s - %s'% (my_inspection.headCavID.head_code.upper()
-                                            ,my_inspection.headCavID.cavity_id.capitalize())
-        my_inspection.save()
+        web_app_user = WebAppEmployee(user=user_obj,EmpManNum=man_num)
+        web_app_user.save()
+    except Exception as e:
+        print str(e)
