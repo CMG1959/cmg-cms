9bd65d5b74da90e44dd8bc7b4d23eb1fa3cf1a87
Michael Caro <mcaro1987@gmail.com>
2016-02-02 09:28:34 -0500

    Add employee at workstation to admin

--- employee/admin.py
+++ employee/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from employee.models import Employees, WebAppEmployee
+from employee.models import Employees, WebAppEmployee, EmployeeAtWorkstation
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
@@ -25,3 +25,10 @@ class EmployeesAdmin(admin.ModelAdmin):
     search_fields = ['EmpLName','EmpFName']
 
 admin.site.register(Employees,EmployeesAdmin)
+
+class EmployeeAtWorkstationAdmin(admin.ModelAdmin):
+    ordering = ['workstation']
+    list_display = ('workstation', 'employee')
+    search_fields = ['workstation', 'employee']
+
+admin.site.register(EmployeeAtWorkstation, EmployeeAtWorkstationAdmin)
