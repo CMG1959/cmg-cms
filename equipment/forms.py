b1c3f2a27fb60443e131dd66d5a358fc8dc4f234
mikecaro <mc947@drexel.edu>
2016-02-14 09:21:41 -0800

    Add init to equipment forms so we get more rows

--- equipment/forms.py
+++ equipment/forms.py
@@ -7,9 +7,17 @@ class equipmentPMForm(forms.ModelForm):
         model = EquipmentPM
         fields = ['Date_Performed',  'equipment_ID', 'pm_frequency', 'logged_pm','comments']
 
+    def __init__(self, *args, **kwargs):
+        super(equipmentPMForm, self).__init__(*args, **kwargs)
+        self.fields['comments'].widget.attrs['rows'] = 4
+
 
 class equipmentRepairForm(forms.ModelForm):
     class Meta:
         model = EquipmentRepair
         fields = ['Date_Performed', 'equipment_ID', 'po_num', 'part_supplier', 'part_name', 'part_number', 'part_cost',
                   'part_quantity','comments']
+
+    def __init__(self, *args, **kwargs):
+            super(equipmentRepairForm, self).__init__(*args, **kwargs)
+            self.fields['comments'].widget.attrs['rows'] = 4
