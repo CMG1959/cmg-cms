21909a4eaab9322600f0e156090c4a1dde874947
mikecaro <mc947@drexel.edu>
2016-02-14 09:29:28 -0800

    Add init to equipment forms so we get more rows

--- equipment/forms.py
+++ equipment/forms.py
@@ -6,10 +6,10 @@ class equipmentPMForm(forms.ModelForm):
     class Meta:
         model = EquipmentPM
         fields = ['Date_Performed',  'equipment_ID', 'pm_frequency', 'logged_pm','comments']
+        widgets = {
+            'comments': forms.Textarea(attrs={'rows':10}),
+        }
 
-    def __init__(self, *args, **kwargs):
-        super(equipmentPMForm, self).__init__(*args, **kwargs)
-        self.fields['comments'].widget.attrs['rows'] = 4
 
 
 class equipmentRepairForm(forms.ModelForm):
@@ -17,7 +17,6 @@ class equipmentRepairForm(forms.ModelForm):
         model = EquipmentRepair
         fields = ['Date_Performed', 'equipment_ID', 'po_num', 'part_supplier', 'part_name', 'part_number', 'part_cost',
                   'part_quantity','comments']
-
-    def __init__(self, *args, **kwargs):
-            super(equipmentRepairForm, self).__init__(*args, **kwargs)
-            self.fields['comments'].widget.attrs['rows'] = 4
+        widgets = {
+            'comments': forms.Textarea(attrs={'rows':10}),
+        }
