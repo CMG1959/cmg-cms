95b17254fad871d045f68723edaf2aba1658f87b
mikecaro <mc947@drexel.edu>
2016-02-21 14:37:57 -0800

    Navbar

--- equipment/serializers.py
+++ equipment/serializers.py
@@ -14,7 +14,7 @@ class EquipmentManufacturerSerializer(serializers.ModelSerializer):
 class EquipmentInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = EquipmentInfo
-        fields = ('id', 'equipment_type', 'part_identifier', 'equipment_manufacturer',
+        fields = ('id', 'equipment_type', 'part_identifier', 'manufacturer_name',
                   'serial_number', 'date_of_manufacture', 'is_active')
     # equipment_types = serializers.StringRelatedField(many=False)
     # equipment_manufacturer = serializers.StringRelatedField(many=False)
