cb29355f9ea1adfb36bee77517107f0f395e133a
mikecaro <mc947@drexel.edu>
2016-02-21 17:26:54 -0800

    Navbar

--- equipment/serializers.py
+++ equipment/serializers.py
@@ -21,8 +21,13 @@ class EquipmentInfoSerializer(serializers.ModelSerializer):
     manufacturer_name = EquipmentManufacturerSerializer(many=False)
 
     def create(self, validated_data):
-        # equipment_type = validated_data.pop('equipment_type')
-        # manufacturer_name = validated_data.pop('manufacturer_name')
-        equipment_info = EquipmentInfo.objects.create(**validated_data)
+        equipment_type_val = validated_data.pop('equipment_type')
+        manufacturer_name_val = validated_data.pop('manufacturer_name')
+
+        equipment_type = EquipmentType.objects.get_or_create(equipment_type=equipment_type_val)
+        manufacturer_name = EquipmentManufacturer.objects.get_or_create(manufacturer_name=manufacturer_name_val)
+
+        equipment_info = EquipmentInfo.objects.create(equipment_type=equipment_type,
+                                                      manufacturer_name=manufacturer_name, **validated_data)
         return equipment_info
 
