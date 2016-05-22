ff3cadcc589226f68a819e1eb19feca9eb8e04a5
mikecaro <mc947@drexel.edu>
2016-05-22 11:11:31 -0700

    Navbar

--- inspection/cav_builder.py
+++ inspection/cav_builder.py
@@ -1,39 +1,12 @@
-import pyodbc
-import json
-import os
 from django.db import connection
-#
-# default_db = json.load(open(os.path.join('..','CIMC', 'config.json')))['default_database']
-#
-# # Connection example: Windows, without a DSN, using the Windows SQL Server driver
-# cnxn_string = 'DRIVER={%s};SERVER=%s;PORT=1433;DATABASE=CMG_Mfg;UID=%s;PWD=%s' % (default_db['OPTIONS']['driver'],
-#                                                                                   default_db['HOST'],
-#                                                                                   default_db['USER'],
-#                                                                                   default_db['PASSWORD'])
-# cnxn = pyodbc.connect(cnxn_string)
-#
-#
-# # Opening a cursor
-# cursor = cnxn.cursor()
-#
-# cursor.execute(
-#     """
-#     SELECT * FROM [CMG_Mfg].[dbo].[GetQMSInspDef] (?, ? , ?, ?)
-#     """, 498, 1 ,11 ,7406)
-#
-# row = cursor.fetchone()
-#
-# print row
-#
-# cnxn.close()
 
-def demo():
+def get_qms_insp_def(job_id=498, inspection_type=1, inspection_id=11, man_num=7406):
 
     cursor = connection.cursor()
+    cursor.execute("SELECT * FROM [CMG_Mfg].[dbo].[GetQMSInspDef] (%s, %s , %s, %s)", [job_id, inspection_type,
+                                                                                       inspection_id ,man_num] )
+    columns = [col[0] for col in cursor.description]
+    return dict(zip(columns, cursor.fetchone()))
 
-    cursor.execute("SELECT * FROM [CMG_Mfg].[dbo].[GetQMSInspDef] (%s, %s , %s, %s)", [498, 1 ,11 ,7406] )
 
-    row = cursor.fetchone()
-
-    print row
 
