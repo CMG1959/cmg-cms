388b9bd10b1e1bfc3f98fd0d6bc35478a048bb00
Michael Caro <mcaro1987@gmail.com>
2016-06-13 20:02:38 -0400

    Fix Pas-Fail and Pass/Fail in building

--- inspection/cav_builder.py
+++ inspection/cav_builder.py
@@ -21,6 +21,7 @@ def get_qms_insp_def(job_id=498, inspection_type=1, inspection_id=11, man_num=74
     cursor.execute("SELECT * FROM [CMG_Mfg].[dbo].[GetQMSInspDef] (%s, %s , %s, %s)", [job_id, inspection_type,
                                                                                        inspection_id ,man_num] )
     columns = [col[0] for col in cursor.description]
+
     return dict(zip(columns, cursor.fetchone()))
 
 
--- inspection/urls.py
+++ inspection/urls.py
@@ -18,16 +18,5 @@ urlpatterns = [
     url(r'^itemReport/(?P<itemNumber>[0-9]+(.[0-9]+)+)/$', views.view_itemReport, name='view_itemReport'),
     ### link to show which inspections can be performed on a given job
     url(r'^(?P<jobNumber>[0-9]+(.[0-9]+)+)/$', views.view_detailJob, name='view_detailJob'),
-    ### link to visual inspection form
-    url(r'^(?P<jobNumber>[0-9]+(.[0-9]+)+)/pf/(?P<inspectionName>[^/]+)/$', views.view_pfInspection, name='view_pfInspection'),
-    ### link to view range inspection form
-    url(r'^(?P<jobNumber>[0-9]+(.[0-9]+)+)/rangeInspection/(?P<inspectionName>[^/]+)/$', views.view_rangeInspection, name='view_rangeInspection'),
-    ### link to view text inspection form
-    url(r'^(?P<jobNumber>[0-9]+(.[0-9]+)+)/textInspection/(?P<inspectionName>[^/]+)/$', views.view_textInspection, name='view_textInspection'),
-    ### link to view integer inspection form
-    url(r'^(?P<jobNumber>[0-9]+(.[0-9]+)+)/IntegerInspection/(?P<inspectionName>[^/]+)/$', views.view_IntegerInspection, name='view_IntegerInspection'),
-    ### link to view float inspection form
-    url(r'^(?P<jobNumber>[0-9]+(.[0-9]+)+)/FloatInspection/(?P<inspectionName>[^/]+)/$', views.view_FloatInspection, name='view_FloatInspection'),
-    ### Get a json dict with error information
     url(r'^job_errors/$', views.view_jsonError, name='view_jsonError'),
 ]
