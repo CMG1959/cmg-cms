e3e6486c922b1c0916605f5baa2fe79a10e8a7e0
mikecaro <mc947@drexel.edu>
2016-06-19 09:19:01 -0700

    Start building view for job using inspection 2.0

--- ins/views.py
+++ ins/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 # Create your views here.
 from startupshot.models import MattecProd
-
+from part.models import Part
+from molds import Mold
 
 @login_required
 def view_index(request):
@@ -19,5 +20,22 @@ def view_index(request):
 
 
 def view_job(request):
-    job_number = request.GET.get('job_number','-1')
-    return True
+    job_number = request.GET.get('job_number', '-1')
+
+    job_in_mattec = MattecProd.objects.get(jobNumber=job_number).values('itemNo','moldNumber')
+
+    part_info = Part.objects.get(item_Number=job_in_mattec['itemNo'])
+    mold_info = Mold.objects.get(mold_number=job_in_mattec['moldNumber'])
+
+
+    template = loader.get_template('ins/view.html')
+    context = RequestContext(request, {
+        'part_info': part_info,
+        'mold_info': mold_info
+        # 'pf_inspectionType': pf_inspectionType,
+        # 'range_inspectionType': range_inspectionType,
+        # 'text_inspectionType': text_inspectionType,
+        # 'int_inspectionType': int_inspectionType,
+        # 'float_inspectionType': float_inspectionType
+    })
+    return HttpResponse(template.render(context))
