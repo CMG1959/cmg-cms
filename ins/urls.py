1c73eace1fa8db2639b08076d59ce3a77b15912d
mikecaro <mc947@drexel.edu>
2016-06-19 11:42:34 -0700

    Wire up a URL

--- ins/urls.py
+++ ins/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     ### link to display active jobs
     url(r'^$', views.view_index, name='index'),
     url(r'^view', views.view_job, name='view_job'),
+    url(r'^record', views.view_record_inspection, name='view_record'),
     # url(r'^entry/', views.view_inspection, name='inspection_entry'),
     # ### link to job report
     # url(r'^jobReport/$', views.view_jobReportSearch, name='view_jobReportSearch'),
