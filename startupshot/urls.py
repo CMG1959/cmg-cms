098c69bff4512072f911b6e9b0549358ff6a1efc
mikecaro <mc947@drexel.edu>
2016-08-29 21:13:08 -0700

    Might as well cache the startup shot page

--- startupshot/urls.py
+++ startupshot/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
-
+from django.views.decorators.cache import cache_page
 from . import views
 
 urlpatterns = [
     # ex: /startupshot/
-    url(r'^$', views.viewActive, name='viewActive'),
+    url(r'^$', cache_page(60*60)(views.viewActive), name='viewActive'),
     # ex: /startupshot/view/
-    url(r'^view/$', views.index, name='viewIndex'),
+    url(r'^view/$', cache_page(60*60)(views.index), name='viewIndex'),
     # ex: /startupshot/123-456789/
     url(r'^(?P<part_number>[0-9]+(-[0-9]+)+)/$', views.detailPart, name='detail'),
     # ex: /startupshot/123-456789/results/
