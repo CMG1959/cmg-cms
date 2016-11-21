de8190c0debe8c784fb38c558ef3b867335b7296
MCARO-MBP\Michael <mcaro1987@gmail.com>
2016-11-20 16:36:06 -0800

    use inspection summary cache

--- dashboard/models.py
+++ dashboard/models.py
@@ -23,7 +23,7 @@ class ProductionSummary(models.Model):
     class Meta:
         verbose_name = 'Production Summary'
         verbose_name_plural = 'Production Summary'
-        db_table = 'vInspectionSummary'
+        db_table = 'vInspectionSummaryCache'
 
     inspection_type = models.CharField(verbose_name='Inspection Type', max_length=9, db_column = 'Inspection_Type')
     date_created = models.DateTimeField(verbose_name='Date Created', db_column = 'Date_Created')
