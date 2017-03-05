854ea1cae1d4b574f826c4c997e3a364f1194623
MCARO-MBP\Michael <mcaro1987@gmail.com>
2017-03-05 08:58:15 -0800

    Try sorting cavity IDs. This may take a while to perfect but we should be okay

--- inspection/views.py
+++ inspection/views.py
@@ -63,7 +63,7 @@ def view_job_detail(request):
 
     active_job = startUpShot.objects.filter(jobNumber=job_number,
                                             machNo=machine).last()
-
+#
     if not active_job:
         redirect_url = '{0}?job_number={1}&machine_number={2}'.format(
             reverse('start_up_shot_create_new'), job_number, machine_number)
